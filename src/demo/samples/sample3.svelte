0082eddd5488f516fb2da09260f6978713ff5bed
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2019-07-10 19:23:08 +0200

    refactor: refactoring samples as a demo site able to show multiple samples

---
+++ src/demo/samples/sample3.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Player from "../player";
+  import CommonSquares from "./commonSquares";
+  import { easeInOutCubic } from "../../core/easings";
+
+  async function animation(a) {
+    a.iterate(".square", (a, idx) => {
+      a.animate({
+        left: [0, 500],
+        duration: 100,
+        easing: easeInOutCubic,
+        delay: idx * 40
+      });
+    });
+  }
+</script>
+
+<CommonSquares />
+<Player {animation} />
