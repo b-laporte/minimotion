e44c7c6d43670a8d5da6509ec77e70acf591dade
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2019-07-12 17:39:55 +0200

    feat/demo: adding samples similar to keyframes examples from animejs

---
+++ src/demo/samples/animejs/common/circle.svelte
@@ -0,0 +1,21 @@
+<style>
+  .wrapper {
+    padding: 50px;
+  }
+  .circle {
+    border-radius: 50%;
+    width: 28px;
+    height: 28px;
+    margin: 1px;
+    background-color: #18ff74;
+  }
+  .shadow {
+    position: absolute;
+    opacity: 0.2;
+  }
+</style>
+
+<div class="wrapper">
+  <div class="circle shadow" />
+  <div class="circle el" />
+</div>
