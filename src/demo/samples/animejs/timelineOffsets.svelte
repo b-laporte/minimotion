f5a5b1a2fbee23db7b78ce5cd4f42f21f0660d5e
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2019-07-15 15:52:46 +0200

    feat/demo: adding samples similar to timeline examples from animejs

---
+++ src/demo/samples/animejs/timelineOffsets.svelte
@@ -0,0 +1,22 @@
+<script>
+  // cf https://animejs.com/documentation/#timelineOffsets
+  import Player from "../../player";
+  import Timeline from "./common/timeline";
+  import { easeOutExpo } from "../../../core/easings";
+
+  async function animation(a) {
+    a.defaults({ easing: easeOutExpo, duration: 750 });
+    a.group(async a => {
+      await a.animate({
+        target: ".el.square",
+        translateX: [0, 250],
+        release: -600
+      });
+      a.animate({ target: ".el.circle", translateX: [0, 250] });
+    });
+    a.animate({ target: ".el.triangle", translateX: [0, 250] });
+  }
+</script>
+
+<Timeline />
+<Player {animation} />
