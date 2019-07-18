2e9ea29a80cc6324d6046943d266bd9205088774
Yannick Meine <yannick.meine@gmail.com>
2019-07-18 16:43:50 +0200

    demo: add sidebar

---
+++ src/demo/sidebar.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { DEMOS } from "./samples";
+
+  export let activeDemo;
+</script>
+
+<style>
+  .sidebar {
+    align-self: start;
+    display: grid;
+    grid-auto-flow: row;
+    grid-auto-rows: 1fr;
+    align-items: center;
+
+    max-height: 100%;
+    overflow: auto;
+  }
+
+  .sample {
+    padding: 1em;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .sample:hover {
+    background: lightgrey;
+  }
+
+  .sample.active {
+    background: aliceblue;
+    color: orange;
+    cursor: initial;
+  }
+
+  .sample.active:hover {
+    background: lightblue;
+  }
+</style>
+
+<div class="sidebar">
+	{#each DEMOS as demo}
+		<div
+			class="sample"
+			class:active={demo === activeDemo}
+			on:click={() => activeDemo = demo}
+		>
+			{demo.title}
+		</div>
+	{/each}
+</div>
