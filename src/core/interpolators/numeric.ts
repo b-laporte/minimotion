9172036eef422d0d411b62e027207caecd2c7a76
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2019-08-19 16:24:46 +0200

    fix: use at least the roundLevel of from and to values in numeric interpolator

--- src/core/interpolators/numeric.ts
+++ src/core/interpolators/numeric.ts
@@ -2,8 +2,26 @@ import { getTransformUnit } from "../transforms";
 import { ValueInterpolatorFactory } from "./types";
 import { interpolate } from "./interpolate";
 
-const RX_NUMERIC_PROP = /^(\*=|\+=|-=)?([+-]?[0-9#.]+)(%|px|pt|em|rem|in|cm|mm|ex|ch|pc|vw|vh|vmin|vmax|deg|rad|turn)?$/,
+const RX_NUMERIC_PROP = /^(\*=|\+=|-=)?([+-]?([0-9]*)(\.[0-9]*)?)(%|px|pt|em|rem|in|cm|mm|ex|ch|pc|vw|vh|vmin|vmax|deg|rad|turn)?$/,
   RX_DEFAULT_PX_PROPS = /(radius|width|height|top|left)$/i;
+
+const parseNumericProp = (number: string) => {
+  const result = RX_NUMERIC_PROP.exec(number);
+  if (result) {
+    const [, relOp, strNumber, beforeDot, afterDot, unit] = result;
+    const fractionDigits = afterDot ? afterDot.length - 1 : 0;
+    if (beforeDot || fractionDigits) {
+      const num = +strNumber;
+      return {
+        relOp,
+        num,
+        unit,
+        fractionDigits
+      };
+    }
+  }
+  return null;
+};
 
 export const numericInterpolatorFactory: ValueInterpolatorFactory = (
   propFrom,
@@ -13,23 +31,21 @@ export const numericInterpolatorFactory: ValueInterpolatorFactory = (
   const { propName, type, fromIsDom } = options;
 
   // propTo is a numeric prop - e.g. '20px' or '+=300.3em' or '0.3'
-  const split = RX_NUMERIC_PROP.exec(propTo);
-  if (!split) {
-    return null;
-  }
-  const relOp = split[1];
-  let to = parseFloat(split[2]);
-  let unit = split[3] || "";
-  let roundLevel = 10;
+  const parsedTo = parseNumericProp(propTo);
+  if (!parsedTo) return null;
+  const relOp = parsedTo.relOp;
+  let to = parsedTo.num;
+  let unit = parsedTo.unit || "";
+  let fractionDigits = 1;
 
   // check consistency
-  const split2 = RX_NUMERIC_PROP.exec(propFrom);
-  if (!split2) return null;
-  if (split2[1]) return null; // cannot be relative
-  const fromUnit = split2[3] || "";
+  const parsedFrom = parseNumericProp(propFrom);
+  if (!parsedFrom) return null;
+  if (parsedFrom.relOp) return null; // cannot be relative
+  const fromUnit = parsedFrom.unit || "";
   if (unit && fromUnit && !fromIsDom && fromUnit !== unit) return null; // units have to be the same
   unit = unit || fromUnit; // if unit is not defined in to value, we use from value
-  const from = parseFloat(split2[2]);
+  const from = parsedFrom.num;
 
   if (!unit) {
     // set default unit for common properties
@@ -57,8 +73,14 @@ export const numericInterpolatorFactory: ValueInterpolatorFactory = (
     }
   }
   if (!unit) {
-    roundLevel = 100; // unit-less properties should be rounded with 2 decimals by default (e.g. opacity)
+    fractionDigits = 2; // unit-less properties should be rounded with 2 decimals by default (e.g. opacity)
   }
+  fractionDigits = Math.max(
+    fractionDigits,
+    parsedFrom.fractionDigits,
+    parsedTo.fractionDigits
+  );
+  const roundLevel = 10 ** fractionDigits;
   return {
     getValue(easing: number) {
       return interpolate(from, to, easing, roundLevel) + unit;
