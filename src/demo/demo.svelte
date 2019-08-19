2f5622d73e5cc2af30020fec2824d8d25d94b53b
Yannick Meine <yannick.meine@gmail.com>
2019-08-19 16:11:52 +0200

    fix(demo): main content scrolling to avoid overflow

--- src/demo/demo.svelte
+++ src/demo/demo.svelte
@@ -33,7 +33,6 @@
     font-size: 1;
     margin: 0;
     height: 100vh;
-    width: 100vw;
     color: #ffffff;
     background-color: #222222;
   }
@@ -51,6 +50,7 @@
     display: grid;
     gap: 1em;
     grid-template-rows: auto 1fr;
+    overflow: auto;
   }
 
   .demo-title {
